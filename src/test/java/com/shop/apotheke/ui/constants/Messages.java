717ac6f9a87f3b2032d5ecdd013a65957dac6d08
Marcela Oliveira <magaeu@gmail.com>
2022-08-07 18:23:21 -0300

    add login page tests ui

---
+++ src/test/java/com/shop/apotheke/ui/constants/Messages.java
@@ -0,0 +1,7 @@
+package com.shop.apotheke.ui.constants;
+
+public class Messages {
+    public static final String LOGIN_PAGE_ERROR = "Page not displayed";
+    public static final String LOGO_NOT_DISPLAYED = "Bank logo not displayed";
+    public static final String NOT_EXPECTED_BANK_RATE = "Bank expected rate is not a match";
+}
---
+++ src/test/java/com/shop/apotheke/ui/pages/LoginPage.java
@@ -0,0 +1,48 @@
+package com.shop.apotheke.ui.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static com.shop.apotheke.ui.constants.Locators.*;
+import static com.shop.apotheke.ui.constants.Messages.LOGIN_PAGE_ERROR;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoginPage extends BasePage {
+
+    private static final String SHOP_APOTHEKE_URL = "https://www.shop-apotheke.com";
+    private static final String PAGE_TITLE = "Anmelden";
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void goToLoginPage() {
+        driver.navigate().to(SHOP_APOTHEKE_URL + "/nx/login");
+        assertTrue(driver.getTitle().contains(PAGE_TITLE),
+                LOGIN_PAGE_ERROR);
+        wait.presenceOfElement(BIN_EIN_MENSCH);
+    }
+
+    public void login(String userName, String password) {
+        getUserNameField().sendKeys(userName);
+        getPasswordField().sendKeys(password);
+        getSubmitButton().submit();
+    }
+
+    private WebElement getUserNameField() {
+        return driver.findElement(By.id(USERNAME_FIELD));
+    }
+
+    private WebElement getPasswordField() {
+        return driver.findElement(By.id(PASSWORD_FIELD));
+    }
+
+    private WebElement getSubmitButton() {
+        return driver.findElement(By.id(LOGIN_BUTTON));
+    }
+
+    public boolean getErrorNotification() {
+        return wait.presenceOfElement(ERROR_NOTIFICATION);
+    }
+}
